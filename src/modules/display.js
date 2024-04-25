338b803088175fdaf1d103fe865a0042269252d3
SYW199X <77220396+SYW199X@users.noreply.github.com>
2024-04-26 09:49:59 +1000

    CSIT883 testing

--- src/modules/display.js
+++ src/modules/display.js
@@ -4,8 +4,8 @@ import React, { useEffect, useState, useRef } from 'react';
 
 //API key 393863da5a92eb3da8aad0a004438259
 
-const INTERVAL = 5000;
-const DISPLAY_NUM = 4;
+const INTERVAL = 7000;
+const DISPLAY_NUM = 6;
 
 const HomeDisplay = ( {baseUrl} ) => {
     const [displayThis, setDisplay] = useState(false);
